2e3aca36bae16670631ddba57ac643aed1e75589
i_aryangoyal <aryangoyal2011@gmail.com>
2021-10-14 11:55:40 +0530

    .

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Text("I am Aryan Goyal"),
+        ),
+      ),
+    )
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:intro/home.dart';
+import 'home.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to My First Flutter App'),
-        ),
-        body: const Center(
-          child: Text('I am Aryan Goyal'),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
